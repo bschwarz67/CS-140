c2dcd129138a7c372732f986559154a71837edd1
bschwarz67 <46033664+bschwarz67@users.noreply.github.com>
2019-02-06 18:57:14 -0600

    Update Client.java

--- cs_2150_object_oriented_programming/cs_2150_project_Bryan_Schwarz/src/cs_2150_project_Bryan_Schwarz/Client.java
+++ cs_2150_object_oriented_programming/cs_2150_project_Bryan_Schwarz/src/cs_2150_project_Bryan_Schwarz/Client.java
@@ -4,6 +4,16 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+
+/*
+The client program for this application creates a running media list to be filtered by the user. 
+It displays the filters in use and possible filters that can be used for the data, as well as the data from the file
+that satisfies the filter.
+The user may continually add or remove filters from the filter list. Each time this operation is performed, the media list is updated
+and the new media list satisfying all filters is printed.
+The user may end the program by entering 'exit' as the command.
+*/
+
 public class Client {
 	public static void main(String [] args) {
 		Scanner scnr = new Scanner(System.in);
